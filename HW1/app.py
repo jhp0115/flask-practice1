3be9521f30184e3817e95ac1ddabb51a6aad31fb
박지호 <jhp3194@outlook.com>
2026-09-23 13:16:59 +0900

    main, profile, greet외 모든 라우트 제거

--- HW1/app.py
+++ HW1/app.py
@@ -17,20 +17,6 @@ def about_page():
 def route_sample(name):
     return render_template("greet.html", name=name)
 
-@app.route("/age/<num>")
-def age_any(num):
-    return f"<h1>{num} 살 타입은 {type(num).__name__}</h1>"
-
-@app.route("/age2/<int:num>")
-def age_any2(num):
-    return f"<h1>{num} 살 타입은 {type(num).__name__}</h1>"
-
-@app.route("/hi/<name1>")
-def hi_template_render(name1):
-    print("AA")
-    return render_template("hi.html", name=name1)
-
-# app.run은 맨 아래에 둬야 한다.
 
 if __name__ == "__main__":
     app.run(debug=True)

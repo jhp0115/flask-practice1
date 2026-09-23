d58e92735a704ab6a902590ae2b5cc1380dacf6f
박지호 <jhp3194@outlook.com>
2026-09-23 12:53:41 +0900

    메인 페이지

---
+++ HW1/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+
+@app.route("/") # default는 GET method.
+def hello_world():
+    return render_template("main.html", name="박지호", number="21010856")
+
+
+@app.route("/about")
+def about_page():
+    return "<h1>소개</h1>"
+
+@app.route("/test/<text>")
+def route_sample(text):
+    return f"<h1>{text}</h1>"
+
+@app.route("/age/<num>")
+def age_any(num):
+    return f"<h1>{num} 살 타입은 {type(num).__name__}</h1>"
+
+@app.route("/age2/<int:num>")
+def age_any2(num):
+    return f"<h1>{num} 살 타입은 {type(num).__name__}</h1>"
+
+@app.route("/hi/<name1>")
+def hi_template_render(name1):
+    print("AA")
+    return render_template("hi.html", name=name1)
+
+# app.run은 맨 아래에 둬야 한다.
+
+if __name__ == "__main__":
+    app.run(debug=True)
